0873568e66a56d022fe9a2cbd7bb137d4690a678
AgustinRguez <agus_rodriguez-@hotmail.com>
2021-02-01 22:05:02 -0300

    agrego clase generica

---
+++ genericoVideos/src/genericoVideos/Empleado.java
@@ -0,0 +1,17 @@
+package genericoVideos;
+
+public class Empleado {
+	private String nombre;
+	private int edad;
+	private double salario;
+	
+	public Empleado(String nombre,int edad, double salario) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.salario = salario;
+	}
+	
+	public String dameDatos() {
+		return "El empleado se llama " + nombre + "Tiene " + edad + "años" + "y un sueldo de " + salario;
+	}
+}
---
+++ genericoVideos/src/genericoVideos/Main.java
@@ -0,0 +1,49 @@
+package genericoVideos;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nombres[] = {"YO", "NO", "SOY"};
+		
+		String elementos = MisMatrices.getElementos(nombres);
+		
+		System.out.println(elementos);
+		System.out.println(MisMatrices.getElMasChico(nombres));
+		
+		
+		/*Empleado listaEmpleados[] = {new Empleado("AGUS", 24, 2500), // con empleado no podria llamar "get el mas chico"
+		  															   // xq no implementa la interfaz comparable, pero string si y por eso puedo imprimir.
+									 new Empleado("JOSEFA", 27, 2000),
+									 new Empleado("MARIA", 30, 5000),
+									 new Empleado("MAM", 21, 10000),
+									 new Empleado("YONO", 18, 9000)
+				
+		};
+		System.out.println(MisMatrices.getElementos(listaEmpleados));*/
+	}
+}
+	class MisMatrices{
+		public static <T> String getElementos(T[]a) { //metodo que se adapta a diferentes tipos
+													  //que le pases como parametro	
+			return "El array tiene " + a.length + " elementos";
+		}
+		
+		public static <T extends Comparable> T getElMasChico(T[]b) {
+			T elementoMenor=b[0];
+			if(b==null || b.length ==0) {
+				
+				return null;
+			}
+			for(int i=1;i<b.length;i++) {
+				if(elementoMenor.compareTo(b[i])> 0) {
+					elementoMenor= b[i];
+				}
+			}
+			return elementoMenor;
+		}
+		
+	}
+
